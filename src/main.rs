3c2c467cd5c6dbd15acf9ad6c0af221f2d835749
GamerFromGamerTown <100511215+GamerFromGamerTown@users.noreply.github.com>
2026-01-10 19:01:36 -0900

    Update main.rs
    add display method! and a bunch of other things

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 // main.rs
-#![allow(dead_code)]
 #![allow(unused)]
 // get rid of this during compile time, it's just annoying during prototyping
 
-use rand::{Rng, random_bool, SliceRandom};
+use rand::{random_bool, rng, seq::SliceRandom, prelude::IndexedRandom};
+use colored::Colorize;
+
+
 //#region
 pub const X_MAX: usize      = 10;
 pub const Y_MAX: usize      = 8;
 pub const GRID_SIZE: usize  = X_MAX * Y_MAX;
 pub const PLAYER_NUMBER: usize = 2;
 pub const NUMBANK_SIZE:  usize = usize::div_ceil(GRID_SIZE, PLAYER_NUMBER); // ceiling division!
-pub const HOLE_PROBABILITY: f64 = 0.0; // 0 >= n >= 1
+pub const ROLL_MAX: u8 = 20;
+pub const HOLE_PROBABILITY: f64 = 0.1; // 0 >= n >= 1
 //#endregion
 /* maybe move the above constants into the struct for the grid */
 
 fn main() {
-
+    let mut game = Game::initialize();
+    game.game_loop();
 }
 
 pub const fn get_x(location: usize) -> usize {
@@ -25,72 +29,134 @@ pub const fn get_y(location: usize) -> usize {
     location / X_MAX // this is floor division, apparently
 }
 
-struct Grid{
+#[derive(Clone, Copy)]
+struct Player {
+    id: u8,
+    score: usize,
+    sum_of_rolls: usize,
+    turn: usize, // used to point at numbank, instead of having to pop numbank every time
+    numbank: [u8; NUMBANK_SIZE]
+}
+
+impl Player {
+
+    fn init(id: u8) -> Self {
+        let mut rng = rng();
+        let mut numarray: [u8; NUMBANK_SIZE] = std::array::from_fn(|i| (i as u8 + 2) % ROLL_MAX);
+        numarray.shuffle(&mut rng);
+        let numbank = numarray;
+        
+        Self {
+            id: id,
+            score: 0,
+            sum_of_rolls: 0,
+            turn: 0,
+            numbank: numarray
+        }
+        // probably it's best to use default method, but this'll do
+    }
+
+    // fn copy(){
+        
+    // }
+
+    fn roll(&self) -> u8 {
+        self.numbank[self.turn]
+    }
+
+}
+
+struct Grid {
     values: [u8; GRID_SIZE],
     owners: [u8; GRID_SIZE],
-    takens: [bool; GRID_SIZE], // i'd consider using a u128 instead, but that would only fit a 
-    
-    tile_num: usize,
-    turn: u16,
+    takens: [bool; GRID_SIZE],
+    adjacency: [bool; GRID_SIZE],
+
+    // tile_count: usize,
+    turn: usize,
 }
 
-struct Game{
-    grid: Grid,
-    players: [Player; PLAYER_NUMBER]
-}
-
-impl Game{
-    fn get_valid_moves(&self){
-        self.grid.takens.iter().all(|&tile| !tile);
-    }
-
-    fn make_random_move(&mut self){
-        let mut rng = rng();
-        moves = self.get_valid_moves();
-        let chosen_move = moves.choose(&mut rng);
-        // implement player pls :)
-        self.grid.add(value, owner, chosen_move);
-    }
-
-    fn game_loop(&mut self){
-        while !self.grid.is_terminal(){
-            player.make_move() // implement make_move please :3
-        }
-    }
-}
 
 impl Grid {
-    fn init(&mut self){
-        self.values = [0u8; GRID_SIZE];
-        self.owners = [0u8; GRID_SIZE];
-        self.turn = 0;
+    fn get_neighbors(&mut self, location: usize) -> Vec<usize>{
+        // really verbose and probably not idiotomic 
+        let is_odd = (get_y(location) % 2 == 1);
+        let mut neighbors: Vec<usize> = vec![];
 
+        if (location + 1 < GRID_SIZE) && ((location + 1) % X_MAX != 0) && !self.takens[location + 1] {
+            neighbors.push(location + 1)}       // right neighbor
+        
+        if (location > 0) && (location % X_MAX != 0) && !self.takens[location - 1]{
+            neighbors.push(location - 1)}       // left neighbor
+
+        if location + X_MAX < GRID_SIZE && !self.takens[location + X_MAX] {
+            neighbors.push(location + X_MAX);   // bottom neighbor (1)
+        }
+        
+        if location >= X_MAX && !self.takens[location - X_MAX]  {
+            neighbors.push(location - X_MAX);   // top neighbor (1)
+        }
+
+
+        // maybe just add 1 if odd and subtract 1 if not, making the + or - 1 depend on is_odd
+        if is_odd {
+            if location >= X_MAX && ((location + 1) % X_MAX != 0) && !self.takens[location - X_MAX + 1] {
+                neighbors.push(location - X_MAX + 1)
+            }
+            if location + X_MAX + 1 < GRID_SIZE && ((location + 1) % X_MAX != 0) && !self.takens[location + X_MAX + 1] {
+                neighbors.push(location + X_MAX + 1)
+            }
+        }
+        
+        else {
+            if location >= X_MAX + 1 && (location % X_MAX != 0) && !self.takens[location - X_MAX - 1] {
+                neighbors.push(location - X_MAX - 1)
+            }
+            if location + X_MAX - 1 < GRID_SIZE && (location % X_MAX != 0) && !self.takens[location + X_MAX - 1] {
+                neighbors.push(location + X_MAX - 1)
+            }
+        }
+        
+        neighbors
+    }
+
+    fn init() -> Self {
+        let values = [0u8; GRID_SIZE];
+        let owners = [0u8; GRID_SIZE];
+        let turn: usize = 0;
+        
+        let mut takens: [bool; GRID_SIZE];
+        
         if HOLE_PROBABILITY > 0.0 {
-            let mut rng = rand::rng();
-            self.takens = [false; GRID_SIZE]; // consider switching vec to arc[<t>]; look into it at least
-            self.takens.fill_with(|| {
+            takens = [false; GRID_SIZE]; // consider switching vec to arc[<t>]; look into it at least
+            takens.fill_with(|| {
                 random_bool(HOLE_PROBABILITY)
             });
-            self.tile_num = self.takens.iter().filter(|&b| *b).count();
         }
 
         else {
-            self.takens  = [false; GRID_SIZE];
+            takens  = [false; GRID_SIZE];
         }
+
+        let adj = [false; GRID_SIZE];
+
+
+        Self {
+            values: values,
+            owners: owners,
+            takens: takens,
+            adjacency: adj,
+            turn  : turn,
+        }
+
+
         
     }
 
-    fn add(&mut self, value: u8, owner: u8, location: usize){
-        if self.takens[location] {
-            panic!()
-        }
-        self.values[location] = value;
-        self.owners[location] = owner;
-        self.takens[location] = true;
-        self.update_neighbors(value, owner, location);
-        self.turn += 1;
+    fn is_terminal(&mut self) -> bool {
+        self.takens.iter().all(|&tile| tile)
     }
-    
+
     fn update_neighbors(&mut self, value: u8, owner: u8, location: usize){
         let neighbors = self.get_neighbors(location);
         for neighbor in neighbors{
@@ -110,53 +176,115 @@ impl Grid {
 
     }
 
-    fn get_neighbors(&mut self, location: usize) -> Vec<usize>{
-        // really verbose and probably not idiotomic 
-        let is_odd = (get_y(location) % 2 == 1);
-        let mut neighbors: Vec<usize> = vec![];
+}
 
-        if (location < GRID_SIZE) && !self.takens[location + 1] {
-            neighbors.push(location + 1)}       // right neighbor
-        
-        if location > 0 && !self.takens[location - 1]{
-            neighbors.push(location - 1)}       // left neighbor
+struct Game {
+    grid: Grid,
+    players: [Player; PLAYER_NUMBER]
+}
 
-        if location + X_MAX <= GRID_SIZE && !self.takens[location + X_MAX] {
-            neighbors.push(location + X_MAX);   // bottom neighbor (1)
+impl Game {
+
+    fn add(&mut self, value: u8, owner: u8, location: usize){
+        if self.grid.takens[location] {
+            panic!("AAAHHHH")
         }
-        
-        if location >= X_MAX && !self.takens[location - X_MAX]  {
-            neighbors.push(location - X_MAX);   // top neighbor (1)
-        }
-
-
-        // maybe just add 1 if odd and subtract 1 if not, making the + or - 1 depend on is_odd
-        if is_odd {
-            if location >= X_MAX - 1 && !self.takens[location - X_MAX + 1] {
-                neighbors.push(location - X_MAX + 1)
-            }
-            if location >= X_MAX + 1 && !self.takens[location + X_MAX + 1] {
-                neighbors.push(location + X_MAX + 1)
-            }
-        }
-        
-        else {
-            if location >= X_MAX - 1 && !self.takens[location - X_MAX - 1] {
-                neighbors.push(location - X_MAX - 1)
-            }
-            if location >= X_MAX + 1&& !self.takens[location + X_MAX - 1] {
-                neighbors.push(location + X_MAX - 1)
-            }
-        }
-    
-    return neighbors
-
+        self.grid.values[location] = value;
+        self.grid.owners[location] = owner;
+        self.grid.takens[location] = true;
+        self.grid.update_neighbors(value, owner, location);
+        self.players[(owner - 1) as usize].turn += 1;
+        self.grid.turn += 1;
     }
 
-    fn is_terminal(&mut self) {
-        println!("lala");
+    fn display(&self) {
         
-        self.takens.iter().all(|&tile| tile);
+        let mut board: String = String::from("");
+        for idx in 0..GRID_SIZE{
+            let mut tile = String::from("");
+            let padding = "  ";
+            let taken = self.grid.takens[idx];
+            let owner = self.grid.owners[idx];
+            let value = self.grid.values[idx];
+            let mut v = self.grid.values[idx].to_string();
+
+            let is_hole =Game::is_hole(taken, owner);
+
+            if value < 10 { // 4 -> 04
+                v = "0".to_owned() + &v
+            }
+            v = "P".to_owned() + &v; // p is placeholder for balancing
+
+            if get_x(idx) == 0 {
+                board.push_str("\n");
+                if get_y(idx) % 2 == 1 {
+                    board.push_str(padding)
+                }
+            }
+
+            if owner > 0 {tile = v}
+            if owner == 1 {     tile = tile.blue() .to_string();}
+            else if owner == 2 {tile = tile.red()  .to_string();}
+            else if owner == 3 {tile = tile.green().to_string();}
+            else if is_hole  {  tile = " X ".to_string()}
+            else if !is_hole {  tile = " · ".to_string()}
+
+            board.push_str(&tile);
+            board.push_str(padding);
+        }
+        println!("{}", board)
+    }
+
+    pub fn game_loop(&mut self){
+        while !self.grid.is_terminal() {
+            for p in self.players.into_iter() { // ISSUE--even if game is not terminal, it can be in the next 2 moves, 
+                if !self.grid.is_terminal(){            // which is not accounted for in the for loop. I added a secondary check,
+                self.make_random_move(p)}       // but it is superfluous and should be replaced later
+            }
+        }
+
+        self.display();
+    }
+
+    fn get_valid_moves(&self) -> Vec<usize>{
+        // self.grid.takens.iter()
+        // .filter(|&tile| !tile);
+        let mut valid_moves: Vec<usize> = Vec::with_capacity(GRID_SIZE);
+        for (idx, tile) in self.grid.takens.iter().enumerate(){
+            if *tile == false {
+                valid_moves.push(idx); 
+            }
+        }
+        valid_moves
+    }
+
+    fn initialize() -> Self {
+        // Grid::init();
+        
+        // for (number, player) in self.players.iter_mut().enumerate(){
+        //     player.init(number as u8)
+        // };
+
+        Self {
+            grid: Grid::init(),
+            // FIXME
+            players: [Player::init(1), Player::init(2)], // halfassed temporary solution
+            // TODO
+        }
+    }
+
+    pub fn is_hole(taken: bool, owner: u8) -> bool {
+        taken && owner == 0
+    }
+
+    fn make_random_move(&mut self, player: Player){
+        // let player: Player = &mut self.players[p]; 
+        let mut rng = rng();
+        let moves: Vec::<usize>  = self.get_valid_moves();
+        let chosen_move: usize = *moves.choose(&mut rng).expect("Game is not terminal.");
+        
+        self.add(player.roll(), player.id, chosen_move);
+        self.display();
     }
 
 }
